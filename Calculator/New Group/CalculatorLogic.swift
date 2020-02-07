28144cc293f287b2bb182101b5c5305141743b4e
Josh Reynaldo <joshuaronaldo088@gmail.com>
2020-02-07 17:21:44 +0700

    finish the chapter

--- Calculator/New Group/CalculatorLogic.swift
+++ Calculator/New Group/CalculatorLogic.swift
@@ -10,13 +10,15 @@ import Foundation
 
 struct CalculatorLogic{
     
-    var number: Double?
+    private var number: Double?
+    
+    private var intermediateCalculation : (number1: Double,  calcMethod : String)?
     
     mutating func setNumber(_ number: Double){
         self.number = number
     }
     
-    func calculation(symbol: String) -> Double? {
+    mutating func calculation(symbol: String) -> Double? {
         if let n = number{
             if symbol == "+/-"{
                 return n * -1
@@ -24,12 +26,32 @@ struct CalculatorLogic{
                 return 0
             } else if symbol == "%"{
                 return n / 100
+            } else if symbol == "=" {
+                performTwoNumberCalculation(n2: n)
+            } else {
+                intermediateCalculation = (number1: n, calcMethod: symbol)
             }
             
         }
         return nil
     }
     
-    
+    private func performTwoNumberCalculation(n2: Double) -> Double?{
+        
+        if let n1 = intermediateCalculation?.number1, let operation = intermediateCalculation?.calcMethod {
+            
+            if operation == "+"{
+                return n1+n2
+            } else if operation == "-"{
+                return n1-n2
+            } else if operation == "*"{
+                return n1*n2
+            } else if operation == "/"{
+                return n1/n2
+            }
+        }
+        
+        return nil
+    }
     
 }
