0a0c2baeb4abd5cf813c987cad0876d4e9bb5e8c
Josh Reynaldo <joshuaronaldo088@gmail.com>
2020-02-07 12:10:22 +0700

    add basic function

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,20 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    private var isFinishedTypingNumber: Bool = true
+    
     @IBOutlet weak var displayLabel: UILabel!
     
     
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
-        
-        //What should happen when a non-number button is pressed
+ 
+        isFinishedTypingNumber = true
     
     }
 
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
-        //What should happen when a number is entered into the keypad
+        if let numValue = sender.currentTitle{
+            
+            if isFinishedTypingNumber{
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                displayLabel.text = displayLabel.text! + numValue
+            }
+        }
     
     }
 
